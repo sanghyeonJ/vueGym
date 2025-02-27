5be7358e2cee9e5bc819f60b6b7e76c8fa977eac
jsh-0808 <jsh080000@rootlab.xyz>
2025-02-27 11:51:12 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheHeader from '@/layouts/TheHeader.vue';
+import TheView from '@/layouts/TheView.vue';
+</script>
+
+<template>
+  <div>
+    <TheHeader />
+    <TheView />
+  </div>
+</template>
+
+<style></style>
---
+++ src/layouts/TheView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <h2>hello</h2>
+    </div>
+  </main>
+</template>
+
+<script setup></script>
+
+<style></style>
