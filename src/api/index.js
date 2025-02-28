73afc0fbb3d5c0c150398f66b494d96652a2e259
jsh-0808 <jsh080000@rootlab.xyz>
2025-02-28 17:52:06 +0900

    오류수정

--- src/api/index.js
+++ src/api/index.js
@@ -9,7 +9,7 @@ function create(baseURL, options) {
   return instance;
 }
 
-export const posts = create(`${import.meta.env.VITE_APP_API_URL}/posts`);
+export const posts = create(`${import.meta.env.VITE_APP_API_URL}posts`);
 // development 환경에서는 5000번 포트로 실행
 // production 환경에서는 5001번 포트로 실행
-// import.meta.env.VITE_APP_API_URL 참고하여 환경에 따라 포트 번호 변경
+// env파일에서 import.meta.env.VITE_APP_API_URL 참고하여 환경에 따라 포트 번호 변경
