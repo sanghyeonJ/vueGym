046a12924bde658831d3c96b682cd025880adc98
jsh-0808 <jsh080000@rootlab.xyz>
2025-02-28 11:46:42 +0900

    중첩라우터

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ import PostCreateView from '@/views/posts/PostCreateView.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
 import PostListView from '@/views/posts/PostListView.vue';
 import PostEditView from '@/views/posts/PostEditView.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import NestedView from '@/views/nested/NestedView.vue';
+import NestedOneView from '@/views/nested/NestedOneView.vue';
+import NestedTwoView from '@/views/nested/NestedTwoView.vue';
+import NestedHomeView from '@/views/nested/NestedHomeView.vue';
 
 const routes = [
   { path: '/', component: HomeView, name: 'homeName' },
@@ -15,6 +20,15 @@ const routes = [
   // : 동적 라우팅 => 여러 url을 하나의 컴포넌트에 매핑
   { path: '/posts/:id', component: PostDetailView, name: 'postDetailName' },
   { path: '/posts/:id/edit', component: PostEditView, name: 'postEditName' },
+
+  { path: '/nested', component: NestedView, name: 'nestedName', children: [
+    { path: '', component: NestedHomeView, name: 'nestedHomeName' },
+    { path: 'one', component: NestedOneView, name: 'nestedOneName' },
+    { path: 'two', component: NestedTwoView, name: 'nestedTwoName' },
+  ] },
+
+
+  { path: '/:pathMatch(.*)*', component: NotFoundView, name: 'notFoundName' },
 ];
 
 const router = createRouter({
---
+++ src/views/nested/NestedView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <!-- replace: true => 라우터 이동 시 현재 페이지를 기록하지 않음 -->
+        <RouterLink class="nav-link" :to="{name: 'nestedOneName', replace: true}" active-class="active">Nested One</RouterLink>
+      </li>
+      <li class="nav-item">
+        <RouterLink class="nav-link" :to="{name: 'nestedTwoName', replace: true}" active-class="active">Nested Two</RouterLink>
+      </li>
+    </ul>
+  </div>
+  <hr class="my-4" />
+  <RouterView></RouterView>
+  <!-- TheView 하위의 라우터 뷰 내에서 NestedView 컴포넌트에서 라우터 뷰를 사용할 수 있음 (중첩된 라우터) -->
+</template>
+
+<script setup></script>
+
+<style scoped></style>
