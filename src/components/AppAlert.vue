4c516718d8848b9efde1fa51b0d2e0bb72faa419
jsh-0808 <jsh080000@rootlab.xyz>
2025-03-05 14:42:52 +0900

    transition group 컴포넌트

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,36 +1,56 @@
 <template>
-  <Transition name="slide">
+  <!-- <Transition name="slide">
     <div v-if="show" class="alert app-alert" :class="typeStyle" role="alert">
         {{ message }}
     </div>
-  </Transition>
+  </Transition> -->
+  
+  <!-- <div class="app-alert">
+    <div v-for="({message, type}, index) in items" :key="index" class="alert" :class="typeStyle(type)" role="alert">{{ message }}</div>
+  </div> -->
+  <div class="app-alert">
+    <TransitionGroup name="slide">
+      <div v-for="({message, type}, index) in items" :key="index" class="alert" :class="typeStyle(type)" role="alert">{{ message }}</div>
+    </TransitionGroup>
+  </div>
 </template>
 
 <script setup>
 import { computed } from 'vue';
 
-const props = defineProps({
-    show: {
-        type: Boolean,
-        default: false
-    },
-    message: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        default: 'error',
-        validator: (value) => {
-            return ['success', 'error'].includes(value);
-        }
-        // success, error 둘중 하나의 값일 경우에만 true
-    }
+defineProps({
+  items: {
+    type: Array,
+  }
 })
 
-const typeStyle = computed(() => {
-    return props.type === 'error' ? 'alert-danger' : 'alert-success'
-})
+// 매개변수로 type을 받아야 하기 때문에 computed를 사용할수 없다
+const typeStyle = (type) => {
+    return type === 'error' ? 'alert-danger' : 'alert-success'
+}
+
+// const props = defineProps({
+//     show: {
+//         type: Boolean,
+//         default: false
+//     },
+//     message: {
+//         type: String,
+//         required: true
+//     },
+//     type: {
+//         type: String,
+//         default: 'error',
+//         validator: (value) => {
+//             return ['success', 'error'].includes(value);
+//         }
+//         // success, error 둘중 하나의 값일 경우에만 true
+//     }
+// })
+
+// const typeStyle = computed(() => {
+//     return props.type === 'error' ? 'alert-danger' : 'alert-success'
+// })
 </script>
 
 <style lang="scss" scoped>
@@ -40,7 +60,7 @@ const typeStyle = computed(() => {
     right: 10px;
     z-index: 1000;
 }
-// name이 있는경우 (name)-enter, (name)-leave 형식으로 사용
+// Transition 태그에 name 속성이 있는경우 (name)-enter, (name)-leave 형식으로 사용
 // name이 없는경우 v-enter, v-leave 형식으로 사용
 .slide-enter{
   &-from{
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -37,7 +37,8 @@
         </div>
       </template>
     </PostForm>
-    <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
+    <!-- <AppAlert :show="showAlert" :message="alertMessage" :type="alertType" /> -->
+     <AppAlert :items="alerts"></AppAlert>
   </div>
 </template>
 
@@ -90,15 +91,18 @@ const goDetail = () => {
   });
 };
 
-const showAlert = ref(false);
-const alertMessage = ref('');
-const alertType = ref('');
+// const showAlert = ref(false);
+// const alertMessage = ref('');
+// const alertType = ref('');
+const alerts = ref([]);
 const vAlert = (message, type = 'error') => {
-  showAlert.value = true;
-  alertMessage.value = message;
-  alertType.value = type;
+  alerts.value.push({message, type});
+  // showAlert.value = true;
+  // alertMessage.value = message;
+  // alertType.value = type;
   setTimeout(() => {
-    showAlert.value = false;
+    //showAlert.value = false;
+    alerts.value.shift();
   }, 2000);
 }
 </script>
