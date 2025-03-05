d363ba7ef178012c7be3ca19ce5731cf0772cd24
jsh-0808 <jsh080000@rootlab.xyz>
2025-03-05 14:15:07 +0900

    transition 컴포넌트

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,7 +1,9 @@
 <template>
-    <div v-if="show" class="alert app-alert" :class="styleClass" role="alert">
+  <Transition name="slide">
+    <div v-if="show" class="alert app-alert" :class="typeStyle" role="alert">
         {{ message }}
     </div>
+  </Transition>
 </template>
 
 <script setup>
@@ -26,7 +28,7 @@ const props = defineProps({
     }
 })
 
-const styleClass = computed(() => {
+const typeStyle = computed(() => {
     return props.type === 'error' ? 'alert-danger' : 'alert-success'
 })
 </script>
@@ -38,4 +40,35 @@ const styleClass = computed(() => {
     right: 10px;
     z-index: 1000;
 }
+// name이 있는경우 (name)-enter, (name)-leave 형식으로 사용
+// name이 없는경우 v-enter, v-leave 형식으로 사용
+.slide-enter{
+  &-from{
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  &-active{
+    transition: all 0.5s ease;
+  }
+  &-to{
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.slide-leave{
+  &-from{
+    opacity: 1;
+    transform: translateY(0);
+  }
+  &-active{
+    transition: all 0.5s ease;
+  }
+  &-to{
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+
+}
+
 </style>
